29986689ecdaf9a18fefc391a58e0d356dadac9f
5ameOld <henker_z@gmx.ch>
2018-07-04 22:04:32 +0200

    Changes to Multiplayer

---
+++ Assets/Scripts/MainMenu/LoadSceneOnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+        GameObject.FindGameObjectWithTag("GameController").GetComponent<PNetworkManager>().currentScene = sceneIndex;
+    }
+}
---
+++ Assets/Scripts/MainMenu/StartScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour {
+
+    public GameObject GameControllerGO;
+
+	void Start () {
+        GameControllerGO.SetActive(true);
+	}
+}
